427ddc4c784d70a101432c89e9b56c80dc41daf1
Chintan <mangukiyachintan2021@gmail.com>
2024-10-01 15:45:23 +0530

    Health

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { FormsModule } from '@angular/forms';
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,8 +11,8 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
     new Recipe('Manchurian', "This is simply a test", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCzGbx87i8TCsYsbiuPxv9Eios1qhpPTdVbg&s"),
-    new Recipe('Mutter Paneer', "This is simply a test", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCzGbx87i8TCsYsbiuPxv9Eios1qhpPTdVbg&s"),
-    new Recipe('paav Bhaji', "This is simply a test", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCzGbx87i8TCsYsbiuPxv9Eios1qhpPTdVbg&s"),
+    new Recipe('Mutter Paneer', "This is simply a test", "https://lh5.googleusercontent.com/p/AF1QipPjK9F5PdGyDVNHcmFSg9sdYdjCZOAehVokHEYb=w92-h92-n-k-no"),
+    new Recipe('paav Bhaji', "This is simply a test", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUpH78G3_-KtUlbcBac9__vDAhZbzsKJzsCw&s"),
 
   ];
   @Output('recipeWaSelected') showRecipe= new EventEmitter<Recipe>();
