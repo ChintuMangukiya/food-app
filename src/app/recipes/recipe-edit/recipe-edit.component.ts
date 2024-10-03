e47fb8816a0e461c3446633ecf3490221cdd19bd
Chintan <mangukiyachintan2021@gmail.com>
2024-10-03 18:34:09 +0530

    Last Change

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,18 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
-
   id: number;
   editMode = false;
-
   constructor(private route: ActivatedRoute) { }
-
   ngOnInit(): void {
     this.route.params.subscribe(
       (params: Params) =>{
@@ -20,8 +16,5 @@ export class RecipeEditComponent implements OnInit {
         this.editMode = params['id'] != null;
       }
     )
-
-
   }
-
 }
