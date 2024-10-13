9fcfad106df22406142210be4c271fb873d1c0c8
mang2022 <mangukiyachintan2021@gmail.com>
2024-10-13 16:52:25 +0530

    Added Authentication

--- src/app/Auth/auth.service.ts
+++ src/app/Auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, tap } from 'rxjs/operators';
-import { Subject, throwError } from 'rxjs';
+import { BehaviorSubject, Subject, throwError } from 'rxjs';
 import { User } from './user.model';
 
 export interface AuthResponseData {
@@ -15,14 +15,13 @@ export interface AuthResponseData {
 }
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 
 export class AuthService {
 
-  user = new Subject<User>();
+  user = new BehaviorSubject<User>(null);
 
-  
   constructor(private http: HttpClient) { }
 
   signup(email: string, password: string) {
@@ -97,3 +96,105 @@ export class AuthService {
     return throwError(errorMessage);
   }
 }
+
+/*
+
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, tap } from 'rxjs/operators';
+import { BehaviorSubject, Subject, throwError } from 'rxjs';
+import { User } from './user.model';
+
+export interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthService {
+
+  user = new BehaviorSubject<User>(null);
+
+  constructor(private http: HttpClient) { }
+
+  signup(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCLiY2XR-5VTTxZGAPl3P-Z2cW7w0cDvyE',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(catchError(this.handleError), tap(resData => {
+        this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+      }));
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCLiY2XR-5VTTxZGAPl3P-Z2cW7w0cDvyE',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true,
+      }
+    ).pipe(catchError(this.handleError), tap(resData => {
+      this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+    }));;
+  }
+
+  logout() {
+    this.user.next(null);
+  }
+
+  private handleAuthentication(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number
+  ) {
+    const expirationDate = new Date(
+      new Date().getTime() + expiresIn * 1000
+    );
+    const user = new User(
+      email,
+      userId,
+      token,
+      expirationDate
+    );
+    this.user.next(user);
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occured!';
+
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email already exists';
+        break;
+
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = "This email doesn't exists.";
+        break;
+
+      case 'INVALID_LOGIN_CREDENTIALS':
+        errorMessage = "Invalid Creadentials";
+        break;
+    }
+    return throwError(errorMessage);
+  }
+}
+*/
--- src/shared/data-storage.service.ts
+++ src/shared/data-storage.service.ts
@@ -2,30 +2,48 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Recipe } from "src/app/recipes/recipe.model";
 import { RecipeService } from "src/app/recipes/recipe.service";
-import { map } from 'rxjs/operators';
-import { AuthService } from "src/app/Auth/auth.service";
+import { exhaustMap, map, take } from 'rxjs/operators';
+import { AuthService } from "src/app/Auth/auth.service"; ``
 
 @Injectable({
     providedIn: 'root'
 })
-export class DataStorageService{
-    constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService){}
+export class DataStorageService {
 
 
-    storeRecipes(){
+    constructor(
+        private http: HttpClient,
+        private recipeService: RecipeService,
+        private authService: AuthService
+    ) { }
+
+
+    storeRecipes() {
         const recipes = this.recipeService.getRecipes();
 
-        this.http.put('https://ng-course-recipe-book-13142-default-rtdb.firebaseio.com/recipes.json',recipes).subscribe((res)=>{
+        this.authService.user.pipe(take(1),
+            exhaustMap(user => {
+                return this.http.put('https://ng-course-recipe-book-13142-default-rtdb.firebaseio.com/recipes.json?auth=' + user.token, recipes)
+            })
+        ).subscribe((res) => {
             console.log(res);
         });
+
     }
 
-    fetchRecipes(){
-        return this.http.get<Recipe[]>('https://ng-course-recipe-book-13142-default-rtdb.firebaseio.com/recipes.json').pipe(map(recipes => {
-            return recipes.map(recipe => {
-                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-            });
-        })).subscribe((recipes)=>{
+    fetchRecipes() {
+
+        this.authService.user.pipe(take(1),
+            exhaustMap(user => {
+                console.log(user);
+                return this.http.get<Recipe[]>('https://ng-course-recipe-book-13142-default-rtdb.firebaseio.com/recipes.json?auth=' + user.token)
+            }),
+            map(recipes => {
+                return recipes.map(recipe => {
+                    return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
+                });
+            })
+        ).subscribe((recipes) => {
             this.recipeService.setRecipes(recipes);
         });
     }
